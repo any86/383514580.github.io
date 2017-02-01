da2334d4dca7c291fa69b2b0ba9b8ae0ae81f90d
宁宁 <383514580@qq.com>
2017-02-01 23:32:15 +0800

    list为可复用组件

--- v2/FE/src/components/List.vue
+++ v2/FE/src/components/List.vue
@@ -7,24 +7,13 @@
             @before-enter="beforeEnter" 
             @enter="enter">
 
-            <li v-if="(each * page > i)" v-for="(row , i) in list" :key="i" :data-index="i" class="list-item">
-                <span class="category">[ {{row.category}} ]</span>
-
-                <router-link class="title" :to="{ name: 'detail', params: { id: row.id }}" tag="h1">
-                    {{row.title}}
-                </router-link>
-
-                <p class="time">{{row.create_time}}</p>
-
-                <p class="desc" v-html="row.desc"></p>
-
-                <router-link class="btn-view" :to="{ name: 'detail', params: { id: row.id }}" tag="a">查看全部</router-link>
+            <li v-for="(item , i) in list" :key="i" :data-index="i">
+                <slot name="item" :row="item" :index="i"></slot>  
             </li>
-
         </transition-group>
         
-        <!-- info -->
-        <p v-show="total < page && ready" class="info">没有更多文章喽!</p>
+        <!-- warning -->
+        <p v-show="total < page && !loading" class="warning">没有更多文章喽!</p>
     </div>
 </template>
 
@@ -34,32 +23,11 @@ import Velocity from 'velocity-animate'
 export default {
     name: 'List',
 
-    data(){
-        return {total: 0, each: 3, data: [], ready: false};
-    },
-
-    mounted(){
-        this.$store.dispatch('getList').then(() => {
-            // 获取所有列表数据
-            this.ready = true;
-            this.data = this.$store.state.list;
-            this.total = Math.ceil(this.data.length / this.each);
-        });
-    },
-
-    props: ['page', 'keyword'],
+    props: ['page', 'list', 'loading', 'each'],
 
     computed: {
-        list(){
-            if('' == this.keyword) {
-                return this.data;
-            } else {
-                return this.data.filter(item=>{
-                    if(-1 < item.title.search(this.keyword)){
-                        return item;
-                    }
-                });
-            }
+        total(){
+            return Math.ceil(this.list.length / this.each);
         }
     },
 
@@ -94,87 +62,21 @@ export default {
 }
 </script>
 <style scoped lang=scss>
-.list-item {
-    transition: all 1s;
-    display: inline-block;
-}
-
-.list-enter-active,
-.list-leave-active {
-    transition: all 1s;
-}
-
-.list-enter,
-.list-leave-active {
-    opacity: 0;
-    transform: translateY(-30px);
-}
-
-
-$font_color: #444;
 .com-list {
     will-change: all;
     max-width: 720px;
     margin: 0.15rem auto;
     display: block;
     overflow: hidden;
-    li {
-        padding: 0.3rem 0.15rem;
-        display: block;
-        overflow: hidden;
-        p,
-        a {
-            font-size: 0.14rem;
-        }
-        a:visited {
-            text-decoration: none;
-            outline: none;
-            border: none;
-        }
-        .category {
-            display: inline-block;
-            padding: 0.05rem 0;
-            letter-spacing: 1px;
-            font-size: 0.2rem;
-        }
-        .title {
-            font-size: 0.2rem;
-            color: #333;
-            display: inline-block;
-            letter-spacing: 1px;
-            font-weight: 600;
-            cursor: pointer;
-        }
-        .desc {
-            margin-top: 0.15rem;
-            letter-spacing: 0.01rem;
-            color: $font_color;
+    >ul{
+        >li{
+            padding: 0.3rem 0.15rem;
             display: block;
-            line-height: 1.5;
-            letter-spacing: 1px;
-        }
-        .time {
-            display: block;
-            color: $font_color;
-            letter-spacing: 1px;
-            height: 0.3rem;
-            line-height: 0.3rem;
-        }
-        .btn-view {
-            padding: 0.05rem;
-            margin-top: 0.3rem;
-            width: 0.7rem;
-            background: #999;
-            color: #fff;
-            clear: both;
-            cursor: pointer;
-            display: block;
-            text-align: center;
-            border-radius: 4px;
-            box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
         }
     }
-    >.info {
+
+    >.warning {
         text-align: center;
         font-size: 0.14rem;
         color: #aaa;
--- v2/FE/src/view/Index.vue
+++ v2/FE/src/view/Index.vue
@@ -10,7 +10,7 @@
 
         <!-- 背景 -->
         <!-- slot: background -->
-        <spinner slot="background" class="down-spinner">
+        <spinner slot="background" class="header-spinner">
             {{topSpinnerText}}
         </spinner>
         
@@ -24,8 +24,28 @@
         
         <!-- slot: content -->
         <template slot="content">
-            <!-- 列表 -->
-            <list :keyword="$route.query.keyword" :page="page" @end="isEnd = true">
+            <!-- 列表  -->
+            <list 
+                class="listview" 
+                :each="listEach" 
+                :page="page" 
+                :list="list" 
+                :loading="listLoading"
+                @end="isEnd = true">
+
+                <template scope="props" slot="item">
+                    <span class="category">[ {{props.row.category}} ]</span>
+
+                    <router-link class="title" :to="{ name: 'detail', params: { id: props.row.id }}" tag="h1">
+                        {{props.row.title}}
+                    </router-link>
+
+                    <p class="time">{{props.row.create_time}}</p>
+
+                    <p class="desc" v-html="props.row.desc"></p>
+
+                    <router-link class="btn-view" :to="{ name: 'detail', params: { id: props.row.id }}" tag="a">查看全部</router-link>
+                </template>
             </list>    
 
             <!-- spinner -->
@@ -53,12 +73,28 @@ export default {
     data(){
     	return {
             listData: [],
-            listLength: null,
+            listLoading: true,
+            listEach: 3,
     		y: 0,
             page: 1,
             isEnd: false,
             topSpinnerText: '松开加载'
         };
+    },
+
+    computed: {
+        list(){
+            return this.listData.filter((item, i)=>{
+                    return (this.listEach * this.page > i);
+            });
+        }
+    },
+
+    mounted(){
+        this.$store.dispatch('getList').then(() => {
+            this.listLoading = false;
+            this.listData    = this.$store.state.list;
+        });
     },
 
     methods: {
@@ -99,8 +135,61 @@ export default {
     p{padding: 0.15rem auto;}
 }
 
-
 .down-spinner{
 	margin:0.15rem auto;
 }
+
+/*列表项*/
+$font_color: #444;
+.listview {
+    a,p{font-size: 0.14rem;}
+    a:visited {
+        text-decoration: none;
+        outline: none;
+        border: none;
+    }
+    .category {
+        display: inline-block;
+        padding: 0.05rem 0;
+        letter-spacing: 1px;
+        font-size: 0.2rem;
+    }
+    .title {
+        font-size: 0.2rem;
+        color: #333;
+        display: inline-block;
+        letter-spacing: 1px;
+        font-weight: 600;
+        cursor: pointer;
+    }
+    .desc {
+        margin-top: 0.15rem;
+        letter-spacing: 0.01rem;
+        color: $font_color;
+        display: block;
+        line-height: 1.5;
+        letter-spacing: 1px;
+    }
+    .time {
+        display: block;
+        color: $font_color;
+        letter-spacing: 1px;
+        height: 0.3rem;
+        line-height: 0.3rem;
+    }
+    .btn-view {
+        padding: 0.05rem;
+        margin-top: 0.3rem;
+        width: 0.7rem;
+        background: #999;
+        color: #fff;
+        clear: both;
+        cursor: pointer;
+        display: block;
+        text-align: center;
+        border-radius: 4px;
+        box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.1);
+    }
+}
+
 </style>
