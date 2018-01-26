dcb78c3cd1f41ce6fd47725880a30a72835f68af
宁宁 <383514580@qq.com>
2018-01-26 14:19:48 +0800

    :zap: switch

--- atom-home/src/router/index.js
+++ atom-home/src/router/index.js
@@ -1,16 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/pages/HelloWorld';
 import Install from '@/pages/Install';
-
+import SwitchDemo from '@/pages/Switch';
 
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-            path: '/hello',
-            name: 'HelloWorld',
-            component: HelloWorld
+            path: '/switch',
+            name: 'Switch',
+            component: SwitchDemo
         },
         {
             path: '/install',
